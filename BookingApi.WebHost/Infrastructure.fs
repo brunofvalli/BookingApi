4c68ac6683dcac707348c4647f2b2613bc1c104a
Mark Seemann <mark@ploeh.dk>
2013-09-20 16:41:19 +0200

    Added Global.asax and Global class.

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -1,1 +1,7 @@
 namespace Ploeh.Samples.Booking.HttpApi.HttpHost
+
+open System
+
+type Global() =
+    inherit System.Web.HttpApplication()
+    member this.Application_Start (sender : obj) (e : EventArgs) = ()
