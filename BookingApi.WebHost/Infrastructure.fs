e7d49f8c6c94b18b76929af2db185707466406f4
Mark Seemann <mark@ploeh.dk>
2013-09-20 16:44:13 +0200

    Added default route.

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -1,7 +1,14 @@
 namespace Ploeh.Samples.Booking.HttpApi.HttpHost
 
 open System
+open System.Web.Http
+
+type HttpRouteDefaults = { Controller : string; Id : obj }
 
 type Global() =
     inherit System.Web.HttpApplication()
-    member this.Application_Start (sender : obj) (e : EventArgs) = ()
+    member this.Application_Start (sender : obj) (e : EventArgs) =
+        GlobalConfiguration.Configuration.Routes.MapHttpRoute(
+            "DefaultAPI",
+            "{controller}/{id}",
+            { Controller = "Home"; Id = RouteParameter.Optional }) |> ignore
