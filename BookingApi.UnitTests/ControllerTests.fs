54c240d02c1a1859ecbf87dddb6b77b7c643547b
Mark Seemann <mark@ploeh.dk>
2013-09-21 13:21:12 +0200

    Moved HomeController tests into a separate module,
    in order to avoid test name clashes when testing similar things for other
    controllers.

--- BookingApi.UnitTests/ControllerTests.fs
+++ BookingApi.UnitTests/ControllerTests.fs
@@ -1,4 +1,4 @@
-﻿module Ploeh.Samples.Booking.HttpApi.UnitTests.ControllerTests
+﻿namespace Ploeh.Samples.Booking.HttpApi.UnitTests
 
 open System.Net.Http
 open System.Web.Http
@@ -6,17 +6,18 @@ open Ploeh.Samples.Booking.HttpApi.Controllers
 open Ploeh.Samples.Booking.HttpApi.UnitTests.Infrastructure
 open Xunit
 
-[<Fact>]
-let SutIsController() =
-    let sut = pool<HomeController> |> Seq.head
-    Assert.IsAssignableFrom<ApiController>(sut)
+module HomeControllerTests =
+    [<Fact>]
+    let SutIsController() =
+        let sut = pool<HomeController> |> Seq.head
+        Assert.IsAssignableFrom<ApiController>(sut)
 
-[<Fact>]
-let GetReturnsCorrectResult() =
-    let sut = pool<HomeController> |> Seq.head
+    [<Fact>]
+    let GetReturnsCorrectResult() =
+        let sut = pool<HomeController> |> Seq.head
     
-    let actual : HttpResponseMessage = sut.Get()
+        let actual : HttpResponseMessage = sut.Get()
     
-    Assert.True(
-        actual.IsSuccessStatusCode,
-        "Actual status code: " + actual.StatusCode.ToString())
+        Assert.True(
+            actual.IsSuccessStatusCode,
+            "Actual status code: " + actual.StatusCode.ToString())
