31a7ca66804ad36bb0da7caa74016b48f0a29d5c
Mark Seemann <mark@ploeh.dk>
2013-09-24 08:52:07 +0200

    Added Availability module,
    in order to encapsulate some availability logic there.

---
+++ BookingApi.UnitTests/DomainModelTests.fs
@@ -0,0 +1,25 @@
+﻿namespace Ploeh.Samples.Booking.HttpApi.UnitTests
+
+open System
+open Ploeh.Samples.Booking.HttpApi.UnitTests.Infrastructure
+open Xunit
+open Xunit.Extensions
+
+module AvailabilityTests =
+    open Ploeh.Samples.Booking.HttpApi.Availability
+
+    [<Theory; TestConventions>]
+    let DatesFromReturnsCorrectSequence (dates : DateTime seq) =
+        let sortedDates =
+            dates |> Seq.map (fun d -> d.Date) |> Seq.sort |> Seq.toList
+        let head = sortedDates |> List.head
+        let last = sortedDates |> Seq.nth 1
+
+        let actual : DateTime seq = DatesFrom head
+
+        Assert.Equal(head, actual |> Seq.head)
+        Assert.Equal(last.AddDays -1.0, actual
+                                        |> Seq.takeWhile (fun d -> d <> last)
+                                        |> Seq.toList
+                                        |> List.rev
+                                        |> List.head)
---
+++ BookingApi/DomainModel.fs
@@ -0,0 +1,8 @@
+﻿namespace Ploeh.Samples.Booking.HttpApi
+
+open System
+
+module Availability =
+    let DatesFrom (date : DateTime) =
+        date |> Seq.unfold (fun d -> Some(d, d.AddDays 1.0))
+
