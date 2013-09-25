794a61c58e1ff4e3295bdbdd748edaf797ad2c94
Mark Seemann <mark@ploeh.dk>
2013-09-25 21:12:32 +0200

    Configured all infrastructure from web host.

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -9,4 +9,4 @@ type HttpRouteDefaults = { Controller : string; Id : obj }
 type Global() =
     inherit System.Web.HttpApplication()
     member this.Application_Start (sender : obj) (e : EventArgs) =
-        ConfigureRoutes GlobalConfiguration.Configuration
+        Configure GlobalConfiguration.Configuration
