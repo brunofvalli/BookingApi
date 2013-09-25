1798d6be93a58f8be8888a7b52b3ca4ba08aa9a2
Mark Seemann <mark@ploeh.dk>
2013-09-25 08:44:39 +0200

    Added a smoke test for single year availability.

---
+++ BookingApi.BoundaryTests/AvailabilityJsonTests.fs
@@ -0,0 +1,14 @@
+﻿module Ploeh.Samples.Booking.HttpApi.BoundaryTests.AvailabilityJsonTests
+
+open System.Net.Http
+open Xunit
+open Xunit.Extensions
+
+[<Theory; BoundaryTestConventions>]
+let GetYearReturnsCorrectResponse(client : HttpClient,
+                                  year : int) =
+    let response = client.GetAsync(sprintf "availability/%i" year).Result
+
+    Assert.True(
+        response.IsSuccessStatusCode,
+        sprintf "Actual status code: %O" response.StatusCode)
--- BookingApi/InfraStructure.fs
+++ BookingApi/InfraStructure.fs
@@ -1,14 +1,44 @@
 ﻿module Ploeh.Samples.Booking.HttpApi.InfraStructure
 
+open System
 open System.Web.Http
+open System.Web.Http.Dispatcher
+open System.Web.Http.Controllers
 open Ploeh.Samples.Booking.HttpApi
+
+type CompositionRoot() =
+    interface IHttpControllerActivator with
+        member this.Create(request, controllerDescriptor, controllerType) =
+            if controllerType = typeof<HomeController> then
+                new HomeController() :> IHttpController
+            elif controllerType = typeof<ReservationsController> then
+                new ReservationsController() :> IHttpController
+            elif controllerType = typeof<AvailabilityController> then
+                new AvailabilityController(10) :> IHttpController
+            else
+                raise
+                <| ArgumentException(
+                    sprintf "Unknown controller type requested: %O" controllerType,
+                    "controllerType")
+
+let ConfigureServices (config : HttpConfiguration) =
+    config.Services.Replace(
+        typeof<IHttpControllerActivator>,
+        CompositionRoot())
 
 type HttpRouteDefaults = { Controller : string; Id : obj }
 
 let ConfigureRoutes (config : HttpConfiguration) =
     config.Routes.MapHttpRoute(
+        "AvailabilityYear",
+        "{controller}/{year}",
+        { Controller = "Home"; Id = RouteParameter.Optional }) |> ignore
+
+    config.Routes.MapHttpRoute(
         "DefaultAPI",
         "{controller}/{id}",
         { Controller = "Home"; Id = RouteParameter.Optional }) |> ignore
 
-let Configure = ConfigureRoutes
+let Configure config =
+    ConfigureRoutes config
+    ConfigureServices config
