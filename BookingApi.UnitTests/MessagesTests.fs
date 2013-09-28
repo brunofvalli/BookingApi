b6efce38de7545b3f2490b341d8c9edc0d51942a
Mark Seemann <mark@ploeh.dk>
2013-09-28 12:40:09 +0200

    Added Envelope module.

---
+++ BookingApi.UnitTests/MessagesTests.fs
@@ -0,0 +1,16 @@
+﻿module Ploeh.Samples.Booking.HttpApi.UnitTests.MessagesTests
+
+open System
+open Ploeh.Samples.Booking.HttpApi
+open Ploeh.Samples.Booking.HttpApi.UnitTests.TestDsl
+open Xunit
+open Xunit.Extensions
+
+[<Theory; TestConventions>]
+let EnvelopReturnsCorrectResult(item : obj,
+                                id : Guid,
+                                created : DateTimeOffset) =
+    let actual = Envelop id created item
+
+    let expected = { Id = id; Created = created; Item = item }
+    Assert.Equal(expected, actual)
--- BookingApi/Messages.fs
+++ BookingApi/Messages.fs
@@ -8,3 +8,16 @@ type MakeReservationCommand = {
     Name : string
     Email : string
     Quantity : int }
+
+[<AutoOpen>]
+module Envelope =
+    [<CLIMutable>]
+    type Envelope<'T> = {
+        Id : Guid
+        Created : DateTimeOffset
+        Item : 'T }
+
+    let Envelop id created item = {
+        Id = id
+        Created = created
+        Item = item }
