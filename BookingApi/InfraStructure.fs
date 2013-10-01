c98743cafe88836a149608fda89f2a427aedd17f
Mark Seemann <mark@ploeh.dk>
2013-10-01 10:02:35 +0200

    Added an agent to coordinate the reservations.

--- BookingApi/InfraStructure.fs
+++ BookingApi/InfraStructure.fs
@@ -1,23 +1,47 @@
 ﻿module Ploeh.Samples.Booking.HttpApi.InfraStructure
 
 open System
+open System.Net.Http
 open System.Web.Http
 open System.Web.Http.Dispatcher
 open System.Web.Http.Controllers
 open Ploeh.Samples.Booking.HttpApi
+open Ploeh.Samples.Booking.HttpApi.Reservations
+
+type Agent<'T> = Microsoft.FSharp.Control.MailboxProcessor<'T>
 
 type CompositionRoot() =
     let seatingCapacity = 10
-    let reservations = [] |> Reservations.ToReservations
+    let reservations =
+        System.Collections.Concurrent.ConcurrentBag<Envelope<Reservation>>()
+
+    let agent = new Agent<Envelope<MakeReservation>>(fun inbox ->
+        let rec loop () =
+            async {
+                let! cmd = inbox.Receive()
+                let rs = reservations |> ToReservations
+                let handle = Handle seatingCapacity rs
+                let newReservations = handle cmd
+                match newReservations with
+                | Some(r) -> reservations.Add r
+                | _ -> ()
+                return! loop() }
+        loop())
+    do agent.Start()
 
     interface IHttpControllerActivator with
         member this.Create(request, controllerDescriptor, controllerType) =
             if controllerType = typeof<HomeController> then
                 new HomeController() :> IHttpController
             elif controllerType = typeof<ReservationsController> then
-                new ReservationsController() :> IHttpController
+                let c = new ReservationsController()
+                let sub = c.Subscribe (EnvelopWithDefaults >> agent.Post)
+                request.RegisterForDispose sub
+                c :> IHttpController
             elif controllerType = typeof<AvailabilityController> then
-                new AvailabilityController(reservations, seatingCapacity) :> IHttpController
+                new AvailabilityController(
+                    reservations |> ToReservations,
+                    seatingCapacity) :> IHttpController
             else
                 raise
                 <| ArgumentException(
