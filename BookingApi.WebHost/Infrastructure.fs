0f56f248fe18b792e6a538ab57f1a9207a2aacfe
Mark Seemann <mark@ploeh.dk>
2013-10-02 09:12:24 +0200

    Refactored agent to raise an event upon acceptance.

--- BookingApi.WebHost/Infrastructure.fs
+++ BookingApi.WebHost/Infrastructure.fs
@@ -1,6 +1,7 @@
 namespace Ploeh.Samples.Booking.HttpApi.HttpHost
 
 open System
+open System.Reactive
 open System.Web.Http
 open Ploeh.Samples.Booking.HttpApi
 open Ploeh.Samples.Booking.HttpApi.Reservations
@@ -16,6 +17,10 @@ type Global() =
         let seatingCapacity = 10
         let reservations =
             System.Collections.Concurrent.ConcurrentBag<Envelope<Reservation>>()
+
+        let reservationEvents = new Subjects.Subject<Envelope<Reservation>>()
+        reservationEvents.Subscribe (fun r -> reservations.Add r) |> ignore
+
         let agent = new Agent<Envelope<MakeReservation>>(fun inbox ->
             let rec loop () =
                 async {
@@ -24,13 +29,12 @@ type Global() =
                     let handle = Handle seatingCapacity rs
                     let newReservations = handle cmd
                     match newReservations with
-                    | Some(r) -> reservations.Add r
+                    | Some(r) -> reservationEvents.OnNext r
                     | _ -> ()
                     return! loop() }
             loop())
         do agent.Start()
-        let agentAsObserver =
-            System.Reactive.Observer.Create (fun cmd -> agent.Post cmd)
+        let agentAsObserver = Observer.Create (fun cmd -> agent.Post cmd)
 
         Configure
             (reservations |> ToReservations)
